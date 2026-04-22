e24217027ac3aaf5bb7e2cf434556c423da7864c
Abhishek lohia <abhisheklohia@tripxl.com>
2026-04-22 17:28:21 +0530

    add flow data send to LLM

--- server.ts
+++ server.ts
@@ -1,48 +1,50 @@
-import express, { type Request, type Response } from "express"
+import express, { type Request, type Response } from "express";
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import graphMethod from "./src/graph";
-dotenv.config()
-const app = express()
+import type { StreamMessage } from "./types";
+dotenv.config();
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/health', (req: Request, res: Response) => {
-    res.send("i am good")
-})
+app.get("/health", (req: Request, res: Response) => {
+  res.send("i am good");
+});
 
 app.post("/chat", async (req: Request, res: Response) => {
-    const data = req.body;
+  const data = req.body;
 
-    res.writeHead(200, {
-        "Content-Type": "text/event-stream",
-        // "Cache-Control": "no-cache, no-transform",
-        // Connection: "keep-alive",
-    });
-    try {
-        const stream = await graphMethod(data);
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    // "Cache-Control": "no-cache, no-transform",
+    // Connection: "keep-alive",
+  });
+  try {
+    const stream = await graphMethod(data);
 
-        for await (const [eventType, chunk] of stream) {
-            const text = JSON.stringify(chunk[0].content, null, 2);
-            console.log('evenType', eventType)
-            console.log(text)
-
-            let message = {type:"ai",payload:chunk[0].content}
-              res.write(`event: ${eventType}\n`);
-              res.write(`data: ${JSON.stringify(message)}\n\n`);
-        }
-
-        // res.write(`event: end\n`);
-        // res.write(`data: done\n\n`);
-        res.end();
-    } catch (error: any) {
-        res.write(`event: error\n`);
-        res.write(`data: ${JSON.stringify(error?.message || "Unknown error")}\n\n`);
-        res.end();
+    for await (const [eventType, chunk] of stream) {
+      const text = JSON.stringify(chunk[0].content, null, 2);
+      const messageType = chunk[0].type;
+      let message: StreamMessage = {} as StreamMessage;
+      if (messageType === "ai") {
+        message = { type: "ai", payload: { text: chunk[0].content as string } };
+      }
+      res.write(`event: ${eventType}\n`);
+      res.write(`data: ${JSON.stringify(message)}\n\n`);
     }
+
+    // res.write(`event: end\n`);
+    // res.write(`data: done\n\n`);
+    res.end();
+  } catch (error: any) {
+    res.write(`event: error\n`);
+    res.write(`data: ${JSON.stringify(error?.message || "Unknown error")}\n\n`);
+    res.end();
+  }
 });
 
 app.listen(8080, () => {
-    console.log(`server is ready`)
-})
+  console.log(`server is ready`);
+});
