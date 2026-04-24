170fa54b3b6651aff436c6040f91c88be4548444
Abhishek lohia <abhisheklohia@tripxl.com>
2026-04-24 22:22:26 +0530

    add validator for login and register

---
+++ src/validators/register.ts
@@ -0,0 +1,23 @@
+import { body } from "express-validator";
+
+export const registerValidator = [
+  body("fullName")
+    .notEmpty()
+    .withMessage("First name is required"),
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid email format"),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .withMessage("Password must be at least 6 characters long"),
+
+  body("role")
+    .notEmpty()
+    .withMessage("Role is required")
+    .isIn(["admin", "manager", "user"])
+    .withMessage("Role must be admin, manager or user"),
+];
